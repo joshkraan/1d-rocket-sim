803c16aad75d0a85aef7dc214b2c40bdf53abc8c
Josh Kraan <joshua.kraan.jk@gmail.com>
2020-08-29 00:09:56 -0700

    Cleaned up gas_properties, heat_flux not currently functional (main needs rewrite)

--- gas_properties.py
+++ gas_properties.py
@@ -1,13 +1,8 @@
 import numpy as np
 import scipy.optimize
-from scipy.interpolate import interp1d
-from scipy.optimize import brentq
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pandas as pd
 import cantera as ct
-import math
-import time
 
 import downsized2 as inp
 import engine_geometry as geom
@@ -16,71 +11,7 @@ sns.set()
 sns.set_style("ticks")
 
 
-def stagnation_properties():
-    gas = ct.Solution('lox_kero.cti')
-    gas.basis = "mass"
-    of = inp.lox_flow_rate / inp.fuel_flow_rate
-    fuel_fraction = 1 / (1 + of)
-    ox_fraction = 1 - fuel_fraction
-
-    throat_area = np.pi * geom.throat_radius ** 2
-    mass_flow_rate = inp.lox_flow_rate + inp.fuel_flow_rate
-
-    def residual(p):
-        gas.TPY = 195, p, 'RP-1:' + str(fuel_fraction) + ',' + 'O2(L):' + str(ox_fraction)
-        gas.equilibrate('HP')
-        gas_constant = ct.gas_constant / gas.mean_molecular_weight
-        g = gas.cp / gas.cv
-        return throat_area * p * np.sqrt(g * (2 / (g + 1)) ** ((g + 1) / (g - 1)) / (gas_constant * gas.T)) - mass_flow_rate
-
-    stagnation_pressure = scipy.optimize.fsolve(residual, inp.chamber_pressure)
-    gas.TPY = 195, stagnation_pressure, 'RP-1:' + str(fuel_fraction) + ',' + 'O2(L):' + str(ox_fraction)
-    gas.equilibrate('HP')
-
-    return gas, stagnation_pressure
-
-
-def exit_pressure_estimate(p_0, gamma):
-    exit_mach = mach_number(gamma, geom.diverging_end)
-    return p_0 * (1 + (gamma - 1) / 2 * exit_mach ** 2) ** (- gamma / (gamma - 1))
-
-
-def calc_gas_properties(pos):
-    stagnation_gas, p_0 = stagnation_properties()
-
-    entropy = stagnation_gas.entropy_mass
-
-    gas = ct.Solution('lox_kero.cti')
-    gas.basis = "mass"
-    gas.state = stagnation_gas.state
-    states = ct.SolutionArray(gas)
-
-    ma = np.zeros(pos.size, dtype=np.float)
-
-    estimated_exit_pressure = exit_pressure_estimate(p_0, stagnation_gas.cp / stagnation_gas.cv)
-
-    for i in range(pos.size):
-
-        def residual(p):
-            gas.SP = entropy, p
-            if inp.equilibrium_expansion:
-                gas.equilibrate('SP')
-            gamma = gas.cp / gas.cv
-            mach = mach_number(gamma, pos[i])
-            return (1 + (gamma - 1) / 2 * mach ** 2) ** (- gamma / (gamma - 1)) - p / p_0
-
-        pressure = brentq(residual, p_0, 0.9 * estimated_exit_pressure)
-        gas.SP = entropy, pressure
-        if inp.equilibrium_expansion:
-            gas.equilibrate('SP')
-
-        states.append(gas.state)
-        ma[i] = mach_number(gas.cp / gas.cv, pos[i])
-
-    return stagnation_gas, states, ma
-
-
-def calc_gas_properties1(pos, lox_flow_rate=inp.lox_flow_rate, fuel_flow_rate=inp.fuel_flow_rate):
+def calc_gas_properties(pos, lox_flow_rate=inp.lox_flow_rate, fuel_flow_rate=inp.fuel_flow_rate):
     stagnation_gas, throat_pressure = solve_chamber_throat(lox_flow_rate, fuel_flow_rate)
 
     gas = ct.Solution('lox_kero.cti')
@@ -88,7 +19,9 @@ def calc_gas_properties1(pos, lox_flow_rate=inp.lox_flow_rate, fuel_flow_rate=in
     gas.state = stagnation_gas.state
     states = ct.SolutionArray(gas)
 
-    estimated_exit_pressure = exit_pressure_estimate(stagnation_gas.P, stagnation_gas.cp / stagnation_gas.cv)
+    gamma = stagnation_gas.cp / stagnation_gas.cv
+    exit_mach = mach_number(gamma, geom.diverging_end)
+    estimated_exit_pressure = stagnation_gas.P * (1 + (gamma - 1) / 2 * exit_mach ** 2) ** (- gamma / (gamma - 1))
 
     for i in range(pos.size):
 
@@ -115,8 +48,8 @@ def calc_gas_properties1(pos, lox_flow_rate=inp.lox_flow_rate, fuel_flow_rate=in
     return stagnation_gas, states, velocity
 
 
-# Function is out of date
 def calc_mole_fractions(pos, of):
+
     stagnation_gas = ct.Solution('lox_kero.cti')
     stagnation_gas.basis = "mass"
     stagnation_states = ct.SolutionArray(stagnation_gas)
@@ -126,50 +59,38 @@ def calc_mole_fractions(pos, of):
     states = ct.SolutionArray(gas)
 
     for i in range(of.size):
+        mass_flow = inp.lox_flow_rate + inp.fuel_flow_rate
         fuel_fraction = 1 / (1 + of[i])
         ox_fraction = 1 - fuel_fraction
-        stagnation_gas.TPY = 195, inp.chamber_pressure, 'RP-1:' + str(fuel_fraction) + ',' + 'O2(L):' + str(ox_fraction)
-        stagnation_gas.equilibrate('HP')
+        gas1, throat_pressure = solve_chamber_throat(mass_flow * ox_fraction, mass_flow * fuel_fraction)
+        stagnation_gas.state = gas1.state
         stagnation_states.append(stagnation_gas.state)
-
-        entropy = stagnation_gas.entropy_mass
-
-        p_0 = inp.chamber_pressure
 
         gas.state = stagnation_gas.state
 
-        estimated_exit_pressure = exit_pressure_estimate(p_0, gas.cp / gas.cv)
+        gamma = stagnation_gas.cp / stagnation_gas.cv
+        exit_mach = mach_number(gamma, geom.diverging_end)
+        estimated_exit_pressure = stagnation_gas.P * (1 + (gamma - 1) / 2 * exit_mach ** 2) ** (- gamma / (gamma - 1))
 
         def residual(p):
-            gas.SP = entropy, p
-            gas.equilibrate('SP')
-            gamma = gas.cp / gas.cv
-            mach = mach_number(gamma, pos)
-            return (1 + (gamma - 1) / 2 * mach ** 2) ** (- gamma / (gamma - 1)) - p / p_0
+            gas.SP = stagnation_gas.entropy_mass, p
+            if inp.equilibrium_expansion:
+                gas.equilibrate('SP')
+            vel = (inp.fuel_flow_rate + inp.lox_flow_rate) / (np.pi * geom.radius(pos) ** 2 * gas.density)
+            h0 = gas.enthalpy_mass + vel ** 2 / 2
+            return h0 - stagnation_gas.enthalpy_mass
 
-        pressure = brentq(residual, p_0, 0.9 * estimated_exit_pressure)
-        gas.SP = entropy, pressure
-        gas.equilibrate('SP')
+        upper_bound = stagnation_gas.P if pos < geom.throat_position else throat_pressure
+        lower_bound = throat_pressure if pos < geom.throat_position else 0.9 * estimated_exit_pressure
+        pressure = float(scipy.optimize.root_scalar(residual, bracket=[lower_bound, upper_bound]).root)
+
+        gas.SP = stagnation_gas.entropy_mass, pressure
+        if inp.equilibrium_expansion:
+            gas.equilibrate('SP')
 
         states.append(gas.state)
 
     return stagnation_states, states
-
-
-@np.vectorize
-def mach_number(gamma, position):
-    radius = geom.radius(position)
-    local_area = np.pi * radius ** 2
-    throat_area = np.pi * geom.throat_radius ** 2
-    ratio = local_area / throat_area
-
-    def mach_equation(m):
-        return 1 / m ** 2 * (2 / (gamma + 1) * (1 + 0.5 * (gamma - 1) * m ** 2)) ** ((gamma + 1) / (gamma - 1)) - ratio ** 2
-
-    if position < geom.throat_position:
-        return scipy.optimize.root_scalar(mach_equation, bracket=[1e-6, 1], method='brentq').root
-    else:
-        return scipy.optimize.root_scalar(mach_equation, bracket=[1, 50], method='brentq').root
 
 
 def solve_chamber_throat(lox_flow_rate, fuel_flow_rate):
@@ -227,116 +148,23 @@ def sound_speed(gas):
     return np.sqrt((perturbed_gas.P - gas.P) / (perturbed_gas.density - gas.density))
 
 
+def mach_number(gamma, position):
+    radius = geom.radius(position)
+    local_area = np.pi * radius ** 2
+    throat_area = np.pi * geom.throat_radius ** 2
+    ratio = local_area / throat_area
+
+    def mach_equation(m):
+        return 1 / m ** 2 * (2 / (gamma + 1) * (1 + 0.5 * (gamma - 1) * m ** 2)) ** ((gamma + 1) / (gamma - 1)) - ratio ** 2
+
+    if position < geom.throat_position:
+        return scipy.optimize.root_scalar(mach_equation, bracket=[1e-6, 1], method='brentq').root
+    else:
+        return scipy.optimize.root_scalar(mach_equation, bracket=[1, 50], method='brentq').root
+
+
 def main():
-    # print(solve_chamber_throat()[0].P, solve_chamber_throat()[1])
-    # gas = solve_chamber_throat()[0]
-    # enthalpy = gas.enthalpy_mass
-    # gas.SP = gas.entropy_mass, solve_chamber_throat()[1]
-    # gas.equilibrate('SP')
-    # print(np.sqrt((gas.cp / gas.cv) * gas.P / gas.density))
-    # print((inp.fuel_flow_rate + inp.lox_flow_rate) / (np.pi * geom.throat_radius ** 2 * gas.density))
-    # print(np.sqrt(2 * (enthalpy - gas.enthalpy_mass)))
     position = np.linspace(0, geom.diverging_end, inp.num_stations, dtype=np.double)
-
-    # chamber_pressure = gas.P
-    # entropy = gas.entropy_mass
-    # enthalpy = gas.enthalpy_mass
-    #
-    # def residual(p):
-    #     gas.SP = entropy, p
-    #     if inp.equilibrium_expansion:
-    #         gas.equilibrate('SP')
-    #     vel = (inp.fuel_flow_rate + inp.lox_flow_rate) / (np.pi * geom.radius(position[779]) ** 2 * gas.density)
-    #     h0 = gas.enthalpy_mass + vel ** 2 / 2
-    #     return (h0 - enthalpy) ** 2
-    #
-    # pressure = np.linspace(200000, chamber_pressure, 1000, dtype=np.double)
-    # result = np.zeros(pressure.size)
-    # for i in range(pressure.size):
-    #     result[i] = residual(pressure[i])
-    # plt.plot(pressure, result)
-    # plt.vlines(throat_pressure, -5e4, 5e4)
-    # plt.hlines(0, 6773, chamber_pressure)
-    # plt.show()
-
-    # throat_pressure = brentq(throat_residual, inp.chamber_pressure, 67730)
-    # gas.SP = entropy, throat_pressure
-    # gas.equilibrate('SP')
-    # print((inp.fuel_flow_rate + inp.lox_flow_rate) / (np.pi * geom.throat_radius ** 2 * gas.density))
-    # print(np.sqrt((gas.cp / gas.cv) * gas.P / gas.density))
-    # print(throat_pressure)
-    #
-    # def residual(p):
-    #     gas.SP = entropy, p
-    #     if inp.equilibrium_expansion:
-    #         gas.equilibrate('SP')
-    #     gamma = gas.cp / gas.cv
-    #     velocity = (inp.fuel_flow_rate + inp.lox_flow_rate) / (np.pi * geom.radius(position[770]) ** 2 * gas.density)
-    #     a = np.sqrt((gas.cp / gas.cv) * gas.P / gas.density)
-    #     m = velocity / a
-    #     return (1 + (gamma - 1) / 2 * m ** 2) ** (- gamma / (gamma - 1)) - p / inp.chamber_pressure
-    #
-    # pressure = np.linspace(67730, inp.chamber_pressure, 1000, dtype=np.double)
-    # result = np.zeros(pressure.size)
-    # for i in range(pressure.size):
-    #     result[i] = residual(pressure[i])
-    # plt.plot(pressure, result)
-    # plt.show()
-    #
-    # # t = time.time()
-    #gas, states, mach = calc_gas_properties(position)
-
-    t = time.time()
-    gas1, states1, velocity = calc_gas_properties1(position)
-    print(gas1.thermal_conductivity, states1[781].thermal_conductivity, states1[999].thermal_conductivity)
-    print(time.time() - t)
-    velocity = (inp.fuel_flow_rate + inp.lox_flow_rate) / (np.pi * geom.radius(position) ** 2 * states1.density)
-    re = states1.density * velocity * 2 * geom.radius(position) / states1.viscosity
-    # plt.vlines(geom.throat_position, 200, 500)
-    plt.plot(position, re)
-    # #plt.hlines(throat_pressure, position[0], position[999])
-    plt.show()
-
-
-    # #velocity_mass_1 = (inp.fuel_flow_rate + inp.lox_flow_rate) / (np.pi * geom.radius(position) ** 2 * states.density)
-    # velocity_mass_2 = (inp.fuel_flow_rate + inp.lox_flow_rate) / (np.pi * geom.radius(position) ** 2 * states1.density)
-    # #velocity_energy_1 = np.sqrt(2 * (gas.enthalpy_mass - states.enthalpy_mass))
-    # velocity_energy_2 = np.sqrt(2 * (gas1.enthalpy_mass - states1.enthalpy_mass))
-    # #
-    # #plt.plot(position, velocity_mass_1)
-    # plt.plot(position, velocity_mass_2)
-    # #plt.plot(position, velocity_energy_1, linestyle='--')
-    # plt.plot(position, velocity_energy_2, linestyle=':')
-    #
-    # plt.show()
-
-    # print(time.time() - t)
-    # t = time.time()
-    # gas, states1, mach = calc_gas_properties1(position)
-    # print(time.time() - t)
-    #
-    # plt.plot(position, states.viscosity)
-    # plt.plot(position, states1.viscosity, linestyle=':')
-    # plt.show()
-
-
-    #print(gas.viscosity, states[781].viscosity)
-
-
-    # pressure = np.linspace(1e6, 30000, 1000)
-    # fraction = gas.X
-    # gamma = np.zeros(pressure.size)
-    # gamma_eq = np.zeros(pressure.size)
-    # entropy = gas.entropy_mass
-    # for i in range(pressure.size):
-    #     gas.SPX = entropy, pressure[i], fraction
-    #     gamma[i] = gas.cp / gas.cv
-    #     gas.equilibrate('SP')
-    #     gamma_eq[i] = gas.cp / gas.cv
-    #
-    # plt.plot(pressure, gamma)
-    # plt.plot(pressure, gamma_eq)
-    # plt.show()
 
     # gas = calc_gas_properties(position)[0]
     # temperature = np.linspace(500, 4000, 1000)
@@ -351,19 +179,19 @@ def main():
     # plt.plot(temperature, (viscosity[0] / temperature[0] ** m) * temperature ** m)
     # plt.show()
     #
-    # of = np.linspace(1.5, 2.5, 100)
-    # stagnation_states, states = calc_mole_fractions(geom.diverging_end, of)
-    #
-    # mole_fractions = stagnation_states.X
-    # plt.plot(of, mole_fractions[:, states.species_index('H')], color='black', label='H')
-    # plt.plot(of, mole_fractions[:, states.species_index('O')], color='black', linestyle='--', label='O')
-    # plt.plot(of, mole_fractions[:, states.species_index('OH')], color='black', linestyle='-.', label='OH')
-    # plt.xlabel('O/F Ratio')
-    # plt.ylabel('Mole Fraction')
-    # plt.rcParams["figure.figsize"] = (1, 1)
-    # sns.despine()
-    # plt.legend()
-    # plt.show()
+    of = np.linspace(1.5, 2.5, 100)
+    stagnation_states, states = calc_mole_fractions(geom.diverging_end, of)
+
+    mole_fractions = stagnation_states.X
+    plt.plot(of, mole_fractions[:, states.species_index('H')], color='black', label='H')
+    plt.plot(of, mole_fractions[:, states.species_index('O')], color='black', linestyle='--', label='O')
+    plt.plot(of, mole_fractions[:, states.species_index('OH')], color='black', linestyle='-.', label='OH')
+    plt.xlabel('O/F Ratio')
+    plt.ylabel('Mole Fraction')
+    plt.rcParams["figure.figsize"] = (1, 1)
+    sns.despine()
+    plt.legend()
+    plt.show()
     #
     # flame_temp = states.T
     # plt.plot(of, flame_temp)
--- heat_flux.py
+++ heat_flux.py
@@ -1,13 +1,7 @@
 import numpy as np
-import scipy.optimize
-from scipy.interpolate import interp1d
-from scipy.optimize import brentq
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pandas as pd
 import cantera as ct
-import math
-import time
 
 import downsized2 as inp
 import engine_geometry as geom
@@ -51,7 +45,7 @@ def cea_bartz(pos, gas_wall_temp):
     return htc
 
 
-def bartz(pos, gas, mach_number, gas_wall_temp):
+def bartz(pos, gas, velocity, gas_wall_temp):
 
     radius = geom.radius(pos)
     local_area = np.pi * radius ** 2
@@ -61,6 +55,8 @@ def bartz(pos, gas, mach_number, gas_wall_temp):
     throat_velocity = mass_flow_rate / (throat_area * gas.density)
     re = gas.density * throat_velocity * inp.throat_diameter / gas.viscosity
     pr = gas.cp * gas.viscosity / gas.thermal_conductivity
+
+    mach_number = velocity / gas_properties.sound_speed(gas)
 
     m = 0.75
     gamma = gas.cp / gas.cv
@@ -72,27 +68,7 @@ def bartz(pos, gas, mach_number, gas_wall_temp):
     return htc
 
 
-def bartz_free_stream(pos, gas, states, mach, gas_wall_temp):
-
-    am_gas = ct.Solution("lox_kero.cti")
-    am_gas.basis = "mass"
-    am_states = ct.SolutionArray(am_gas, pos.size)
-    am_states.TPX = states.TPX
-    am_states.TP = (states.T + gas_wall_temp) / 2, states.P
-
-    velocity = mach * np.sqrt((states.cp / states.cv) * states.P / states.density)
-
-    re = gas.density * velocity * 2 * geom.radius(pos) / gas.viscosity
-    pr = gas.cp * gas.viscosity / gas.thermal_conductivity
-
-    sigma = (am_states.density / gas.density) ** 0.8 * (am_states.viscosity / gas.viscosity) ** 0.2
-
-    nu = 0.026 * re ** 0.8 * pr ** 0.4 * (inp.throat_diameter / inp.throat_bevel_radius) ** 0.1 * sigma
-    htc = nu * gas.thermal_conductivity / (2 * geom.radius(pos))
-    return htc
-
-
-def bartz_free_stream1(pos, gas, states, velocity, gas_wall_temp):
+def bartz_free_stream(pos, gas, states, velocity, gas_wall_temp):
 
     am_gas = ct.Solution("lox_kero.cti")
     am_gas.basis = "mass"
@@ -109,68 +85,33 @@ def bartz_free_stream1(pos, gas, states, velocity, gas_wall_temp):
     return htc
 
 
-def dittus_boelter(pos, states, mach, gas_wall_temp, prandtl_correction, stagnation_temp):
-    pr_free_stream = states.cp * states.viscosity / states.thermal_conductivity
+def dittus_boelter(pos, states, velocity, gas_wall_temp):
+    am_gas = ct.Solution("lox_kero.cti")
+    am_gas.basis = "mass"
+    am_states = ct.SolutionArray(am_gas, pos.size)
+    am_states.TPX = (states.T + gas_wall_temp) / 2, states.P, states.X
 
-    if prandtl_correction:
-        reference_temp = 0.5 * (states.T + gas_wall_temp) + 0.22 * pr_free_stream ** (1/3) * (stagnation_temp - states.T)
-    else:
-        reference_temp = 0.5 * (states.T + gas_wall_temp)
-
-    ref_gas = ct.Solution("lox_kero.cti")
-    ref_gas.basis = "mass"
-    ref_states = ct.SolutionArray(ref_gas, pos.size)
-    ref_states.TPX = states.TPX
-    ref_states.TP = reference_temp, states.P
-
-    velocity = mach * np.sqrt((states.cp / states.cv) * states.P / states.density)
-
-    re = ref_states.density * velocity * 2 * geom.radius(pos) / ref_states.viscosity
-    pr = ref_states.cp * ref_states.viscosity / ref_states.thermal_conductivity
+    re = am_states.density * velocity * 2 * geom.radius(pos) / am_states.viscosity
+    pr = am_states.cp * am_states.viscosity / am_states.thermal_conductivity
 
     nu = 0.023 * re ** 0.8 * pr ** 0.4
-    htc = nu * ref_states.thermal_conductivity / (2 * geom.radius(pos))
+    htc = nu * am_states.thermal_conductivity / (2 * geom.radius(pos))
     return htc
 
 
-def sieder_tate(pos, states, mach, gas_wall_temp):
-
-    wall_gas = ct.Solution("lox_kero.cti")
-    wall_gas.basis = "mass"
-    wall_states = ct.SolutionArray(wall_gas, pos.size)
-    wall_states.TPX = states.TPX
-    wall_states.TP = gas_wall_temp, states.P
-
-    diameter = 2 * geom.radius(pos)
-
-    velocity = mach * np.sqrt((states.cp / states.cv) * states.P / states.density)
-
-    re = states.density * velocity * diameter / states.viscosity
-    pr = states.cp * states.viscosity / states.thermal_conductivity
-
-    nu = 0.027 * re ** 0.8 * pr ** (1/3) * (states.viscosity / wall_states.viscosity) ** 0.14
-
-    htc = nu * states.thermal_conductivity / diameter
-
-    return htc
-
-
-def sieder_tate1(pos, states, velocity, gas_wall_temp):
+def sieder_tate(pos, states, velocity, gas_wall_temp):
 
     wall_gas = ct.Solution("lox_kero.cti")
     wall_gas.basis = "mass"
     wall_states = ct.SolutionArray(wall_gas, pos.size)
     wall_states.TPX = gas_wall_temp, states.P, states.X
 
-    diameter = 2 * geom.radius(pos)
-
-    re = states.density * velocity * diameter / states.viscosity
+    re = states.density * velocity * 2 * geom.radius(pos) / states.viscosity
     pr = states.cp * states.viscosity / states.thermal_conductivity
 
     nu = 0.027 * re ** 0.8 * pr ** (1/3) * (states.viscosity / wall_states.viscosity) ** 0.14
 
-    htc = nu * states.thermal_conductivity / diameter
-
+    htc = nu * states.thermal_conductivity / (2 * geom.radius(pos))
     return htc
 
 
